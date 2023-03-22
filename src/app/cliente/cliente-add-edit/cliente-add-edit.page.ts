8eba7c2eb830113dcac49f05e651048ab185a483
Bernardo Gularte Kirsch <bernardogulartekirsch@gmail.com>
2023-03-21 21:21:14 -0300

    alteração para mensagem de campos obrigatórios

--- src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
+++ src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
@@ -14,7 +14,31 @@ export class ClienteAddEditPage implements OnInit {
 
   clienteForm!: FormGroup;
 
+  hasErrors = false;
+  errorMessage: string[] | undefined;
+
   constructor(private formBuilder: FormBuilder) { }
+
+  validationMessages = {
+    nome: [
+      { type: 'required', message: 'Nome é obrigatório' },
+      { type: 'minlength', message: 'Nome deve ter ao menos 3 caracteres' },
+      { type: 'maxlength', message: 'Nome não pode ter mais que 50 caracteres' }
+    ],
+    email: [
+      { type: 'required', message: 'E-mail é obrigatório' },
+      { type: 'email', message: 'Informe um e-mail com formato válido' }
+    ],
+    tel: [
+      { type: 'required', message: 'Telefone é obrigatório' }
+    ],
+    renda: [
+      { type: 'min', message: 'Renda precisa ser positiva' }
+    ],
+    nascimento: [
+      { type: 'required', message: 'Data de nascimento é obrigatória' }
+    ]
+  }
 
   ngOnInit() {
     this.clienteForm = this.formBuilder.group({
@@ -40,9 +64,23 @@ export class ClienteAddEditPage implements OnInit {
   public nome: string | undefined;
 
   submit() {
-    if (this.clienteForm.invalid || this.clienteForm.pending) {
-      return;
+    /* this.errorMessage = [];
+    if (this.clienteForm.get('nome')!.hasError('required')) {
+      this.errorMessage.push('Nome é obrigatório');
     }
+    if (this.clienteForm.get('email')!.hasError('required')) {
+      this.errorMessage.push('Email é obrigatório');
+    }
+    if (this.clienteForm.get('tel')!.hasError('required')) {
+      this.errorMessage.push('Telefone é obrigatório');
+    }
+    if (this.clienteForm.get('renda')!.hasError('required')) {
+      this.errorMessage.push('Renda é obrigatória');
+    }
+    if (this.clienteForm.get('nascimento')!.hasError('required')) {
+      this.errorMessage.push('Data de nascimento é obrigatória');
+    }
+    this.hasErrors = this.errorMessage.length > 0; */
   }
 
 }
