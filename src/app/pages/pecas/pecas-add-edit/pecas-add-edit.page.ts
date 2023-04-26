57d296586146058db270442728cdc28ba9d740db
Bernardo Gularte Kirsch <bernardogulartekirsch@gmail.com>
2023-04-25 21:34:23 -0300

    adição, alteração e remoção de itens

--- src/app/pages/pecas/pecas-add-edit/pecas-add-edit.page.ts
+++ src/app/pages/pecas/pecas-add-edit/pecas-add-edit.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Peca } from 'src/app/models/peca.model';
 import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Guid } from 'guid-typescript';
 import { ToastService } from 'src/app/services/toast.service';
+import { PecasService } from 'src/app/services/pecas.services';
 
 @Component({
   templateUrl: './pecas-add-edit.page.html'
@@ -17,6 +18,8 @@ export class PecasAddEditPage implements OnInit {
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private toastService: ToastService,
+    private pecasService: PecasService,
+    private router: Router,
   ) { }
   
   iniciarEdicao() {
@@ -29,16 +32,20 @@ export class PecasAddEditPage implements OnInit {
   }
 
   async submit() {
+    await this.pecasService.update(this.pecasForm.value);
+    this.peca = this.pecasForm.value;
+
     // Atualizar/Inserir objeto de maneira persistida
     this.toastService.presentToast('Gravação bem sucedida', 3000, 'top');
     this.modoDeEdicao = false;
     // Navegar para a página principal
+    this.router.navigateByUrl('');
   }
 
-  ngOnInit() {
+  async ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     if (id && Guid.isGuid(id)) {
-      // Recuperaremos o objeto persistido
+      this.peca = await this.pecasService.getById(id);
     } else {
       this.peca = {id: Guid.createEmpty(), nome: '', valor: 0.00 };
       this.modoDeEdicao = true;
