1fec45239acbd911cb7d5d95be8244ae7bbf89ea
Bernardo Gularte Kirsch <bernardogulartekirsch@gmail.com>
2023-05-04 22:56:05 -0300

    alterações para usar o telefone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,38 @@
 import { Storage } from '@ionic/storage-angular';
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { DatabaseService } from './services/database.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent {
+  private initPlugin!: boolean;
   constructor(
     private storage: Storage,
-  ) {}
+    private platform: Platform,
+    private databaseService: DatabaseService,
+  ) {
+    this.initializeApp();
+  }
+  
+  async initializeApp() {
+    this.platform.ready().then(async () => {
+      this.databaseService.initializePlugin().then(async (ret) => {
+        try {
+          const db = await this.databaseService.createConnection("oficina", false, "no-encryption", 1);
+          this.initPlugin = ret;
+        } catch (err) {
+          console.log(`Error: ${err}`);
+          this.initPlugin = false;
+        }
+        console.log('Status da inicialização do plugin: ' + this.initPlugin);
+      });
+    });
+  }
 
   async ngOnInit() {
     await this.storage.create();
